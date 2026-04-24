b44d47a8975122fb445065cbcc0ab4ede6f6137b
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2026-04-24 16:14:12 -0300

    Use a "real" equation (broken)

--- src/routes/tp1/+page.ts
+++ src/routes/tp1/+page.ts
@@ -6,8 +6,8 @@ export const load = (async () => {
 			{
 				name: 'Orange',
 				color: 'oklch(70.5% 0.213 47.604)',
-				max_v: 5,
-				max_i: 25,
+				max_v: 3,
+				max_i: 20,
 				values: [
 					{ v: 0.0, i: 0.0 },
 					{ v: 0.47, i: 0.0 },
@@ -22,11 +22,9 @@ export const load = (async () => {
 					{ v: 3.0, i: 13.7 },
 					{ v: 3.1, i: 20.0 }
 				],
-				vtoi(v?: number) {
-					const volts = v ?? 0;
-
+				vtoi(volts: number) {
 					if (volts < 2.52) return 0;
-					return (volts - 2.52) * 10;
+					return 0.188 - 0.0179 * volts + 0.551 * volts ** 2;
 				}
 			}
 		] as const
